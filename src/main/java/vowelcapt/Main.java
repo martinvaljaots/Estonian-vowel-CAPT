bfb0f8fdb7614c3ac0a5c7e587da5d74963f8561
Martin.Valjaots <martin.valjaots@ttu.ee>
2018-02-23 13:57:17 +0200

    Add believable first formant finding, example WAVs
    Second and third formants still broken

--- src/main/java/vowelcapt/Main.java
+++ src/main/java/vowelcapt/Main.java
@@ -1,5 +1,6 @@
 package vowelcapt;
 
+import be.tarsos.dsp.resample.SoundTouchRateTransposer;
 import de.fau.cs.jstk.exceptions.MalformedParameterStringException;
 import de.fau.cs.jstk.framed.*;
 import de.fau.cs.jstk.io.FrameOutputStream;
@@ -10,34 +11,34 @@ import vowelcapt.panels.Spectrogram;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        String[] trial = new String[]{"Record-002.wav", "2"};
+        String[] trial = new String[]{"Record-i.wav", "3"};
         try {
             AudioSource as = new AudioFileReader(trial[0],
                     RawAudioFormat.create(trial.length > 2 ? trial[1] : "f:" + trial[0]),
                     true);
             Window wnd = new HammingWindow(as, 25, 10, false);
-            AutoCorrelation acf = new FastACF(wnd);
-            LPCSpectrum lpc = new LPCSpectrum(acf);
+            // AutoCorrelation acf = new FastACF(wnd);
+            AutoCorrelation acf = new SimpleACF(wnd);
+            LPCSpectrum lpc = new LPCSpectrum(acf, 22, true);
             Formants fs = new Formants(lpc, as.getSampleRate(), Integer.parseInt(trial[1]));
-            System.out.println(as);
-            System.out.println(wnd);
+            System.out.println(as.getSampleRate());
+            System.out.println(Arrays.toString(trial));
+            System.out.println(wnd.getFrameSize());
             System.out.println(acf);
-            System.out.println(lpc);
+            System.out.println(lpc.getFrameSize());
             System.out.println(fs);
-            double [] buf = new double [fs.getFrameSize()];
+            double[] buf = new double[fs.getFrameSize()];
             FrameOutputStream fos = new FrameOutputStream(buf.length);
-// TODO: saada aru, millised formandid on millised. tundub, et
-            // küll on aga võimalik siiski saada esimesed n formanti - hetkel saabki 3.
+// TODO: leida, mis värk on sellega, et teine ja kolmas formant pole ÜLDSE usutavate väärtustega
 
             while (fs.read(buf)) {
-                for (double d : buf) {
-                    System.out.println(d + " ");
-                }
+                System.out.println(Arrays.toString(buf));
             }
 
         } catch (UnsupportedAudioFileException e) {
